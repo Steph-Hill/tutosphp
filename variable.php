a7382126a58bdfe1c313bc8902445bd4d65c59f5
Steph-Hill <stephen_hillion@outlook.fr>
2022-03-09 10:42:00 -0500

    first commit

---
+++ variable.php
@@ -0,0 +1,36 @@
+<?php
+
+    $prenom = "Stéphen";
+
+    $nom = "HILLION";
+    
+    $age = "27";
+
+    $affAge = true;
+
+
+
+
+    $affSortie = $nom." ".$prenom ;
+
+    if($affAge){
+
+        $affSortie = $affSortie ." vous avez ". $age. " ans." ;
+
+    }
+
+    echo $affSortie;
+
+    /*echo $nom
+            ." "
+            .$prenom
+            . "  vous avez "
+            . $age
+            ." ans."
+            ." félicitation!!!!";*/
+
+
+
+
+
+?>
